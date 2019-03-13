ad5ed0f2dea010bb8f50788c1f73cb15146f789d
Egor Kolyshkin <egor.kolyshkin@mail.ru>
2019-03-13 23:18:04 +0300

    bezier realization

---
+++ src/Bezier.hs
@@ -0,0 +1,32 @@
+module Bezier where
+
+import Engine
+{-
+	Bezier curves
+	n - polynom grade	
+	P - (n + 1) points
+	B(t) = sum from 0 to n with k 
+			Pk * bkn(t) where
+				0 <= t <= 1
+				bkn(t) = Cnk * (t ^ k) * (1 - t) ^ (n - k)
+-}
+
+b' :: Int -> [Point Float] -> Float -> Point Float
+b' n points t | length points /= n + 1 = error "IllEgal points list"
+			  | otherwise = ifoldr (accumulate t n) (Point 0.0 0.0) points
+
+accumulate :: Float -> Int -> Int -> Point Float -> Point Float -> Point Float
+accumulate t n k acc new = (translate acc) $ (scale (b n k t) new) 
+
+b :: Int -> Int -> Float -> Float
+b n k t = (fromIntegral (cnk n k)) * (t ^ k) * (1.0 - t)
+
+factorial :: Int -> Int
+factorial n = foldl (*) 1 [1..n]
+
+cnk :: (Int) -> (Int) -> (Int)
+cnk n k = div (factorial n) ((factorial k) * (factorial (n - k)))
+
+ifoldr :: (Int -> a -> b -> b) -> b -> [a] -> b
+ifoldr f z xs = foldr (\x g i -> f i x (g (i+1))) (const z) xs 0
+
--- src/Main.hs
+++ src/Main.hs
@@ -1,17 +1,33 @@
 module Main(main) where
 
 import Graphics.Gloss
+import Bezier
+import Engine
 
 width, height, offset :: Int
 width = 300
 height = 300
 offset = 100
 
+path = map toBezierPoint [1..100]
+lineBezier = line path
+
+points = [Point 0.0 0.0, Point 0.0 1.0, Point 1.0 1.0, Point 1.0 0.0]
+n = 3
+
+bezier = b' n points
+
+toBezierPoint n = let point = bezier (n * 0.01) in ((fromIntegral width) * ((x point) - 0.5), ( fromIntegral height) * ((y point) - 0.5))
+
+
+
 window :: Display
-window = InWindow "Pong" (width, height) (offset, offset)
+window = InWindow "Pong" (600, 600) (offset, offset)
 
 background :: Color
 background = white
 
 main :: IO ()
-main = display window background (circle 50)
+main = display window background lineBezier
+
+
