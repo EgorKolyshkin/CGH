f2d703075b482cc14f81f6ee1e7bbf37318934bc
Egor Kolyshkin <egor.kolyshkin@mail.ru>
2019-03-13 20:53:53 +0300

    added gloss dependency

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,17 @@
-module Main where
+module Main(main) where
+
+import Graphics.Gloss
+
+width, height, offset :: Int
+width = 300
+height = 300
+offset = 100
+
+window :: Display
+window = InWindow "Pong" (width, height) (offset, offset)
+
+background :: Color
+background = white
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = display window background (circle 50)
