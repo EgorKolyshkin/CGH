e716fe16b1575e631bf7f723fe4d5a7d8aa3e64f
Yaroslav Atroshenko <yaaroslavok@gmail.com>
2019-04-11 21:05:40 +0300

    implements 3d plane rotating

--- src/Main.hs
+++ src/Main.hs
@@ -5,21 +5,27 @@ import Bezier
 import Engine
 import PlaneRotate
 
-resultPlane t = line $ planeToPath (doWithPlaneInOrigin plane (rotatePlane rotate3DX (realToFrac t)))
+offsetX = -0.5
+offsetY = -0.5
 
-planeToPath (Plane fst snd thd fth) = [toPair fst, toPair snd, toPair thd, toPair fth]
+resultPlane t = line $ planeToPath (aggregatedPlane (doWithPlaneInOrigin plane (transformPlane t)))
+
+transformPlane :: Float -> Plane -> Plane
+transformPlane t plane = rotatePlane rotate3DY (realToFrac (t / 2.0)) $ rotatePlane rotate3DX (realToFrac t) plane
+
+planeToPath (Plane fst snd thd fth) = [toPair fst, toPair snd, toPair thd, toPair fth, toPair fst]
 
 toPair (Point3D x y z) = (realToFrac x, realToFrac y)
 
-point1 = Point3D 0.1 0.1 0.1
-point2 = Point3D 0.3 0.1 0.1
-point3 = Point3D 0.1 0.5 0.2
-point4 = Point3D 0.3 0.5 0.2
+point1 = Point3D 0.3 0.3 0.1
+point4 = Point3D 0.5 0.3 0.1
+point2 = Point3D 0.3 0.7 0.2
+point3 = Point3D 0.5 0.7 0.2
 
 plane = Plane point1 point2 point3 point4
 
 aggregatedPlane :: Plane -> Plane
-aggregatedPlane plane = scalePlane 600 plane
+aggregatedPlane plane = scalePlane 600 $ translatePlane plane (Point3D offsetX offsetY 0.0) 
 
 
 window :: Display
